b3106bedbeda368767e56b668e0386e6d4396421
l4v <ivos.jovan@gmail.com>
2017-12-19 19:07:40 +0100

    Changes to be committed: 	modified:   FTN_VEZBANJE/me/vezbe/006/p2 	modified:   FTN_VEZBANJE/me/vezbe/006/p2.c

--- FTN_VEZBANJE/me/vezbe/006/p2.c
+++ FTN_VEZBANJE/me/vezbe/006/p2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct darray_st{
 	int *parray;
@@ -24,7 +25,7 @@ void resize_ar(Array *a){
 void add_item(Array *a, int val){
 	if(a->filled == a->capacity)
 		resize_ar(a);
-	
+
 	a->parray[a->filled] = val;
 	a->filled ++;
 }
@@ -39,23 +40,37 @@ void print_ar(Array *a){
 	for(i = 0; i < a->filled; i ++){
 		if(i > 0)
 			printf(", ");
-			
+
 		printf("%i", a->parray[i]);
 	}
 	printf("]\n");
 }
 
-void rm_item(Array *a, int item){
+void rm_item_index(Array **a, int i){
+		Array *temp;
+		temp->parray = malloc((size_ar(*a)-1)*sizeof(int));
+		temp->capacity = (size_ar(*a)-1)*sizeof(int);
+		temp->filled = (*a)->filled - 1;
+		if(i != 0)
+			memcpy(temp->parray, (*a)->parray, (i-1)*sizeof(int));
+		if(i != (*a)->capacity -1)
+			memcpy(temp->parray + (i*sizeof(int)), (*a)->parray + i + 1, (size_ar(*a) - i - 1)*sizeof(int));
+		free((*a)->parray);
+		(*a)->parray = temp->parray;
+}
+
+void rm_item_val(Array *a, int item){
 	int i = 0;
 	for( i = 0; i < a->filled; i ++)
 		if(a->parray[i] == item)
-			free((a->parray[i]);
-	printf("Removed item: %i\n", item);
-	if(a->capacity >= 2*(a->filled)){
-		resize_ar(a);
-		puts("Resized array");
-		printf("New size: %i, of that, used: %i \n", a->capacity, a->filled);
-	}
+			rm_item_index(&a, i);
+
+				printf("Removed item: %i\n", item);
+				if(a->capacity >= 2*(a->filled)){
+					resize_ar(a);
+					puts("Resized array");
+					printf("New size: %i, of that, used: %i \n", a->capacity, a->filled);
+				}
 }
 
 void destroy_ar(Array *a){
@@ -66,20 +81,20 @@ void destroy_ar(Array *a){
 int main(){
 	int i = 0;
 	Array a;
-	
+
 	init_ar(&a);
 	for(i = 0; i < 99; i ++)
 		add_item(&a, i);
 	print_ar(&a);
 	printf("Velicina:%i\n", size_ar(&a));
-	
+
 	add_item(&a, 42);
 	print_ar(&a);
 	printf("Velicina:%i\n", size_ar(&a));
 	printf("Zauzeto:%i\n", a.filled);
-	
-	rm_item(&a, 64);
-	
+
+	rm_item_val(&a, 2);
+
 	destroy_ar(&a);
 	printf("Velicina:%i\n", size_ar(&a));
 	print_ar(&a);
